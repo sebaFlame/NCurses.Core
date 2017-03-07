4ee9cd8492e6ed31c6619920e05d1278d1606a37
C_B-A <C_B-A@3a9065fc-4a7e-7541-9dc8-748989605d84>
2017-03-07 22:21:13 +0000

    Initial dummy panel class
    git-svn-id: file:///H/Users/C_B-A/temp/NCurses.Core_git/trunk@8 3a9065fc-4a7e-7541-9dc8-748989605d84

---
+++ src/NCurses.Core/Panel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NCurses.Core
+{
+    public class Panel : IDisposable
+    {
+        public Window Window { get; set; }
+
+        public Panel(Window window)
+        {
+
+        }
+
+        public void Show()
+        {
+
+        }
+
+        public void Hide()
+        {
+
+        }
+
+        public void SetTop()
+        {
+
+        }
+
+        public static void UpdatePanels()
+        {
+
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
